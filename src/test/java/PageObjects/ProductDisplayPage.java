aacd4699f370df479aad1ec3cfc2e2236f1f96b7
syam.s@outlook.com <Liverp00l>
2016-11-14 10:30:15 +0100

    added PDP , added search result and clicking on the first product.

--- src/test/java/PageObjects/ProductDisplayPage.java
+++ src/test/java/PageObjects/ProductDisplayPage.java
@@ -1,8 +1,13 @@
 package PageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
+
+import java.util.List;
 
 /**
  * Created by syam.suryanarayanan on 11/14/2016.
@@ -10,9 +15,19 @@ import org.testng.asserts.SoftAssert;
 public class ProductDisplayPage {
     WebDriver driver;
     SoftAssert softAssert = new SoftAssert();
+    @FindAll({
+            @FindBy(xpath="//div[@id='product-content']//ul[@class='swatches size']")
+    })
+    public List<WebElement> allSizes ;
+
 
     public ProductDisplayPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
     }
+    /*clicks on the first available size*/
+    public void clickOnFirstSize(){
+        allSizes.get(1).click();
+    }
+
 }
