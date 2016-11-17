a5f5961d763f6b90a360ebafb40a084d868fc71a
syam.s@outlook.com <Liverp00l>
2016-11-17 14:29:08 +0100

    to get to the billing page, i have added a keys.Return, which is not the correct way to do it. I tried waits and  action builder to move to the billing button and click.

--- src/test/java/PageObjects/BillingPage.java
+++ src/test/java/PageObjects/BillingPage.java
@@ -1,6 +1,8 @@
 package PageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
 
@@ -11,6 +13,12 @@ public class BillingPage {
     WebDriver driver;
     SoftAssert softAssert = new SoftAssert();
 
+    @FindBy(id="dwfrm_billing_paymentMethods_creditCard_owner")
+    public WebElement txtBoxCreditCardUserName;
+    @FindBy(id="dwfrm_billing_paymentMethods_creditCard_number_d0vwtozoazfe")
+    public WebElement txtBoxCreditCardNumber;
+    @FindBy(id="dwfrm_billing_paymentMethods_creditCard_cvn_d0omarmoruon")
+    public WebElement txtBoxCVVNumber;
 
 
 
@@ -19,5 +27,16 @@ public class BillingPage {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
     }
+    public void enterName(String name) {
+        txtBoxCreditCardUserName.sendKeys(name);
 
+    }
+    public void enterNumber(String number) {
+        txtBoxCreditCardNumber.sendKeys(number);
+
+    }
+    public void enterCVV(String cvv) {
+        txtBoxCVVNumber.sendKeys(cvv);
+
+    }
 }
