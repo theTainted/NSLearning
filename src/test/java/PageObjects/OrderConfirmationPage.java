0da7cd9d4ab196dd9ece572355f25d1754e41a69
syam.s@outlook.com <Liverp00l>
2016-11-17 15:33:33 +0100

    for select dropdowns i am passing values and treating them as WebElements instead of dropdowns, need to check this again.

--- src/test/java/PageObjects/OrderConfirmationPage.java
+++ src/test/java/PageObjects/OrderConfirmationPage.java
@@ -1,7 +1,37 @@
 package PageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.asserts.SoftAssert;
+
 /**
  * Created by syam.suryanarayanan on 11/17/2016.
  */
 public class OrderConfirmationPage {
+    WebDriver driver;
+    SoftAssert softAssert = new SoftAssert();
+
+  @FindBy(xpath="//div[@id='main']//div[@class='confirmation__message']")
+    public WebElement confirmationMessage;
+
+
+    public OrderConfirmationPage(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(this.driver,this);
+
+    }
+
+    public void confirmOrderConfirmationPage(){
+        if(confirmationMessage.isDisplayed()){
+            softAssert.notify();
+            softAssert.assertAll();
+        }
+        else {
+            softAssert.fail("order confirmation page is not shown");
+            softAssert.assertAll();
+        }
+    }
+
 }
