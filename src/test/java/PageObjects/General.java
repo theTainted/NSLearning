016fd76a1501e3e823adb5198f822066148fe95d
syam.s@outlook.com <Liverp00l>
2016-11-21 13:46:24 +0100

    Need to add the driver in the general class

--- src/test/java/PageObjects/General.java
+++ src/test/java/PageObjects/General.java
@@ -1,5 +1,8 @@
 package PageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by syam.suryanarayanan on 11/21/2016.
  */
@@ -7,5 +10,7 @@ public class General {
 
    public static String URL = "https://" + "storefront" + ":" + "storefront" + "@" + "development.northsails.com/de/en/homepage/";
 
-
 }
+
+
+
---
+++ src/test/java/testcases/RegisteredUserCheckOutFlow.java
@@ -0,0 +1,37 @@
+package testcases;
+
+import PageObjects.General;
+import PageObjects.HomePage;
+import PageObjects.ProductDisplayPage;
+import PageObjects.ProductListPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by syam.suryanarayanan on 11/21/2016.
+ */
+public class RegisteredUserCheckOutFlow {
+
+@Test
+    public void testRegisteredUserCheckOutFlow(){
+    System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+
+    driver.get(General.URL);
+    driver.manage().window().maximize();
+    //Accept cookies message
+    HomePage homePage =new HomePage(driver);
+    homePage.clickAcceptCookiesButton();
+    //Click on ContinueShopping in the countrySelector PopuP
+    homePage.clickShoppingButtonInCountrySelector();
+
+    //Search for Jackets
+    homePage.clickOnSearchIcon();
+    ProductListPage plp= homePage.searchText("JACKET DICKEY");
+    ProductDisplayPage pdp = plp.clickOnFirstResult();
+    pdp.clickOnFirstSize();
+    //Thread.sleep(2000);
+    pdp.clickOnAddToCart();
+}
+}
