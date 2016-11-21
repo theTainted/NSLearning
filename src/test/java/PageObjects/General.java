b61fdfe3a5f7ebc7fbeb618e56eabf03e3bf9b4c
syam.s@outlook.com <Liverp00l>
2016-11-21 14:09:15 +0100

    Need to add the driver in the general class

--- src/test/java/PageObjects/General.java
+++ src/test/java/PageObjects/General.java
@@ -2,13 +2,18 @@ package PageObjects;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 
 /**
  * Created by syam.suryanarayanan on 11/21/2016.
  */
 public class General {
+   public WebDriver driver;
 
    public static String URL = "https://" + "storefront" + ":" + "storefront" + "@" + "development.northsails.com/de/en/homepage/";
+
+
+
 
 }
 
