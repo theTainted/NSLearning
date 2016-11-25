2b6db94ca5822fe195ce67c837070c4a3fefc40c
syam.s@outlook.com <Liverp00l>
2016-11-25 15:49:59 +0100

    Added a loop to run the reegistered user scripts

--- src/test/java/testcases/GuestUserCheckOutFlow.java
+++ src/test/java/testcases/GuestUserCheckOutFlow.java
@@ -19,58 +19,59 @@ public class GuestUserCheckOutFlow extends General{
    // public static void main(String[] args) throws InterruptedException {
     /*016fd76(Need to add the driver in the general class)*/
      public void testGuestUserCheckOutFlow() {
-
+       String[] countries = {"Italy","Germany","France","Spain","United Kingdom","Netherlands","Switzerland","Sweden","Belgium","Greece"};
+       for(int i=0;i<countries.length;i++) {
       /*  System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
         WebDriver driver = new ChromeDriver();*/
-        General.getBrowser("Chrome");
-        driver.get(General.URL);
-        driver.manage().window().maximize();
-    //Accept cookies message
-        HomePage homePage =new HomePage(driver);
-        homePage.clickAcceptCookiesButton();
-    //Click on ContinueShopping in the countrySelector PopuP
-     //   homePage.clickShoppingButtonInCountrySelector();
+          General.getBrowser("Chrome");
+          driver.get(General.URL);
+          driver.manage().window().maximize();
+          //Accept cookies message
+          HomePage homePage = new HomePage(driver);
+          homePage.clickAcceptCookiesButton();
+          //Click on ContinueShopping in the countrySelector PopuP
+          //   homePage.clickShoppingButtonInCountrySelector();
 
-    //Search for Jackets
-        homePage.clickOnSearchIcon();
-        ProductListPage plp= homePage.searchText("jacket dickey");
-        ProductDisplayPage pdp = plp.clickOnFirstResult();
-         pdp.clickOnFirstSize();
-        //Thread.sleep(2000);
-        pdp.clickOnAddToCart();
-        ShoppingBasket sb= pdp.clickOnMiniBasketIcon();
-        Login login = sb.clickOnCheckoutButton();
-        ShippingAddress shippingAddress = login.clickOnCheckoutAsGuest();
-        shippingAddress.selectMan();
-        shippingAddress.enterFirstName("test");
-        shippingAddress.enterLastName("tester");
-        shippingAddress.enterAddressOne("Line 1");
-        shippingAddress.enterZip("1114AB");
-        shippingAddress.enterCity("Amsterdam");
-         shippingAddress.selectCountry("Greece");
-        shippingAddress.enterEmail("bjornjaco.geerding@digitaslbi.com");
-        shippingAddress.enterPhone("3333333333");
-       // shippingAddress.selectBillingAddressOption();
+          //Search for Jackets
+          homePage.clickOnSearchIcon();
+          ProductListPage plp = homePage.searchText("jacket dickey");
+          ProductDisplayPage pdp = plp.clickOnFirstResult();
+          pdp.clickOnFirstSize();
+          //Thread.sleep(2000);
+          pdp.clickOnAddToCart();
+          ShoppingBasket sb = pdp.clickOnMiniBasketIcon();
+          Login login = sb.clickOnCheckoutButton();
+          ShippingAddress shippingAddress = login.clickOnCheckoutAsGuest();
+          shippingAddress.selectMan();
+          shippingAddress.enterFirstName("test");
+          shippingAddress.enterLastName("tester");
+          shippingAddress.enterAddressOne("Line 1");
+          shippingAddress.enterZip("1114AB");
+          shippingAddress.enterCity("Amsterdam");
+          shippingAddress.selectCountry(countries[i]);
+          shippingAddress.enterEmail("bjornjaco.geerding@digitaslbi.com");
+          shippingAddress.enterPhone("3333333333");
+          // shippingAddress.selectBillingAddressOption();
 
-        BillingPage billingPage= shippingAddress.clickBtnToBilling();
+          BillingPage billingPage = shippingAddress.clickBtnToBilling();
 
 
-      //  Thread.sleep(5000);
-        billingPage.enterNumber("4111111111111111");
-        billingPage.enterCVV("737");
-        billingPage.enterName("test");
-        billingPage.selectTermsAndConditions();
-        billingPage.selectExpiryMonth("August");
-        billingPage.selectExpiryYear("2018");
-        billingPage.clickBtnToPay();
+          //  Thread.sleep(5000);
+          billingPage.enterNumber("5100290029002909");
+          billingPage.enterCVV("737");
+          billingPage.enterName("test");
+          billingPage.selectTermsAndConditions();
+          billingPage.selectExpiryMonth("October");
+          billingPage.selectExpiryYear("2020");
+          billingPage.clickBtnToPay();
 
-       driver.quit();
+          driver.quit();
 /*
                 Actions action = new Actions(driver);
         WebElement we = driver.findElement(By.xpath("//div[@id='wrapper']/header//i[@class='icon-bag']"));
         action.moveToElement(we).build().perform();
         driver.findElement(By.xpath("//div[@id='wrapper']/header//a[@class='btn btn--large btn--fullwidth mini-cart-link-checkout']")).click();*/
 
-
+       }
     }
 }
