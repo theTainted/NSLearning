19afe18be13be590a7535c01de724e01bc52599c
syam.s@outlook.com <Liverp00l>
2016-11-30 11:44:24 +0100

    added steps to add items to basket and then register a new user and then checkout.

--- src/test/java/testcases/RegisterAndThenCheckOut.java
+++ src/test/java/testcases/RegisterAndThenCheckOut.java
@@ -1,17 +1,19 @@
 package testcases;
 
 import PageObjects.*;
+import org.testng.annotations.Test;
 
 /**
  * Created by syam.suryanarayanan on 11/30/2016.
  */
 public class RegisterAndThenCheckOut extends General{
-
+@Test
     public void testRegisterAndThenCheckOut() {
         General.getBrowser("Chrome");
         driver.get(General.URL);
         driver.manage().window().maximize();
         HomePage homePage = new HomePage(driver);
+        homePage.clickAcceptCookiesButton();
         homePage.clickOnSearchIcon();
         ProductListPage plp = homePage.searchText("jacket dickey");
         ProductDisplayPage pdp = plp.clickOnFirstResult();
@@ -29,6 +31,33 @@ public class RegisterAndThenCheckOut extends General{
         register.enterPassword();
         register.enterConfrimsPassword();
         register.clickCreateAccount();
+
+        ShippingAddress shippingAddress = new ShippingAddress(driver);
+
+        shippingAddress.selectMan();
+        shippingAddress.enterFirstName("test");
+        shippingAddress.enterLastName("tester");
+        shippingAddress.enterAddressOne("Line 1");
+        shippingAddress.enterZip("1114AB");
+        shippingAddress.enterCity("Amsterdam");
+      //  shippingAddress.selectCountry();  -> by default will place for the selected country
+       // shippingAddress.enterEmail("bjornjaco.geerding@digitaslbi.com"); -> Address section is prefilled with the email used to register
+        shippingAddress.enterPhone("3333333333");
+        // shippingAddress.selectBillingAddressOption();
+
+        BillingPage billingPage = shippingAddress.clickBtnToBilling();
+
+
+        //  Thread.sleep(5000);
+        billingPage.enterNumber("5100290029002909");
+        billingPage.enterCVV("737");
+        billingPage.enterName("test");
+        billingPage.selectTermsAndConditions();
+        billingPage.selectExpiryMonth("October");
+        billingPage.selectExpiryYear("2020");
+        billingPage.clickBtnToPay();
+
+        driver.quit();
 
     }
 }
