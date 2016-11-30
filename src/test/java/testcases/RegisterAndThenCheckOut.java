9d15598c54ad83c8f805c042107079594453cc4e
syam.s@outlook.com <Liverp00l>
2016-11-30 11:17:28 +0100

    Added a new testcase Register and then checkout. RegisterAndThenCheckOut

--- src/test/java/testcases/RegisterAndThenCheckOut.java
+++ src/test/java/testcases/RegisterAndThenCheckOut.java
@@ -1,6 +1,7 @@
 package testcases;
 
 import PageObjects.General;
+import PageObjects.HomePage;
 
 /**
  * Created by syam.suryanarayanan on 11/30/2016.
@@ -8,6 +9,10 @@ import PageObjects.General;
 public class RegisterAndThenCheckOut extends General{
 
     public void testRegisterAndThenCheckOut() {
+        General.getBrowser("Chrome");
+        driver.get(General.URL);
+        driver.manage().window().maximize();
+        HomePage homePage = new HomePage(driver);
 
     }
 }
