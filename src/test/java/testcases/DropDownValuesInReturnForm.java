cce526b6427431894050b1217b22fdd2b7dee127
syam.s@outlook.com <Liverp00l>
2016-12-07 11:53:52 +0100

    added footerReturnLink

--- src/test/java/testcases/DropDownValuesInReturnForm.java
+++ src/test/java/testcases/DropDownValuesInReturnForm.java
@@ -1,10 +1,25 @@
 package testcases;
 
+import PageObjects.General;
+import PageObjects.HomePage;
+import org.testng.annotations.Test;
+
+import static PageObjects.General.driver;
+
+
 /**
  * Created by syam.suryanarayanan on 12/7/2016.
  */
 public class DropDownValuesInReturnForm {
-    public static void main (String[] args){
-        
+@Test
+    public void testDropDownValuesInReturnForm(){
+        General.getBrowser("Chrome");
+        driver.get(General.URL);
+        driver.manage().window().maximize();
+        HomePage homePage = new HomePage(driver);
+        homePage.clickAcceptCookiesButton();
+        homePage.clickLoginRegisterButton();
+    //clicking on returns link in the footer
+        homePage.footerReturnLink.click();
     }
 }
