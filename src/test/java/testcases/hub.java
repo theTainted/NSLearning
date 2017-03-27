17cf44d8cd9771117d0e8955502a0917ceb97ef5
syam.s@outlook.com <Liverp00l>
2017-03-27 12:27:39 +0200

    changed digizuite to hub

--- src/test/java/testcases/hub.java
+++ src/test/java/testcases/hub.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 /**
  * Created by syam.suryanarayanan on 2/22/2017.
  */
-public class digizuite {
+public class hub {
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
