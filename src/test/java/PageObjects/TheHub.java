a9ee90138b66d3c78373a771f46cbda413f5a99f
syam.s@outlook.com <Liverp00l>
2017-03-27 12:41:34 +0200

    Added a new Class file TheHub

--- src/test/java/PageObjects/TheHub.java
+++ src/test/java/PageObjects/TheHub.java
@@ -1,7 +1,28 @@
 package PageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 /**
  * Created by syam.suryanarayanan on 3/27/2017.
  */
 public class TheHub {
+
+    public static WebDriver driver;
+    public static String theHubUrl = "http://test-thehub.endemolshine.com/";
+    public static String userName = "syam.suryanarayanan@digitaslbi.com";
+    public static String password = "P@ssword1";
+
+    @FindBy(id="login-email")
+    public WebElement txtBoxLoginEmail;
+
+    public TheHub(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+    public void setLoginEmail(String userName){
+     txtBoxLoginEmail.sendKeys(userName);
+    }
 }
--- src/test/java/testcases/hub.java
+++ src/test/java/testcases/hub.java
@@ -2,6 +2,9 @@ package testcases;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import PageObjects.TheHub;
+
+
 
 /**
  * Created by syam.suryanarayanan on 2/22/2017.
@@ -10,7 +13,12 @@ public class hub {
     public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("http://test-thehub.endemolshine.com/");
+      TheHub theHub = new TheHub(driver);
+      driver.get(TheHub.theHubUrl);
+      theHub.setLoginEmail(TheHub.userName);
+
+
+
 
     }
 }
