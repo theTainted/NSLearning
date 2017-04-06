81ec77fa9d6163ea4e921060ead4aa6d13caafbd
syam.s@outlook.com <Liverp00l>
2017-04-06 07:52:00 +0200

    Added the test theHubLogin

---
+++ src/test/java/testcases/theHubLogIn.java
@@ -0,0 +1,24 @@
+package testcases;
+
+import PageObjects.TheHub;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by syam.suryanarayanan on 4/6/2017.
+ */
+public class theHubLogIn {
+
+  @Test
+    public void testTheHubLogin(){
+      System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+      WebDriver driver = new ChromeDriver();
+      TheHub theHub = new TheHub(driver);
+      driver.get(TheHub.theHubUrl);
+      driver.manage().window().maximize();
+      theHub.setLoginEmail(TheHub.userName);
+      theHub.setLoginPassword(TheHub.password);
+      theHub.clickOnLoginButton();
+  }
+}
