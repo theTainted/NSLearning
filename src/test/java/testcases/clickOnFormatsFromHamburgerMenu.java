a874831f700cfedbd9de92ffc9fa0fa221f2bfcc
syam.s@outlook.com <Liverp00l>
2017-04-12 09:34:59 +0200

    Added a new class in testcases clickOnFormatsFromHamburgerMenu

---
+++ src/test/java/testcases/clickOnFormatsFromHamburgerMenu.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by syam.suryanarayanan on 4/12/2017.
+ */
+public class clickOnFormatsFromHamburgerMenu {
+    @Test
+    public void testClickFormatsFromHamburgerMenu(){
+
+    }
+
+}
