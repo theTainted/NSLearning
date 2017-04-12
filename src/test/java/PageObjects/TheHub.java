8556c0895df57954876c995aa35ed9d99aa48230
syam.s@outlook.com <Liverp00l>
2017-04-12 13:59:10 +0200

    Added method clickonCalendarFromHamburgerMenu() Added class in test cases clickonCalendarFromHamburgerMenu

--- src/test/java/PageObjects/TheHub.java
+++ src/test/java/PageObjects/TheHub.java
@@ -57,32 +57,39 @@ public class TheHub {
     public WebElement hamburgerCalendarLink;
     @FindBy(xpath="//a[@href='/Brand toolkit']")
     public WebElement hamburgerBrandTookitLink;
+    @FindBy(xpath="//a[@href='/Commercial']")
+    public WebElement hamburgerCommercialLink;
+    @FindBy(xpath="//a[@href='/Policies']")
+    public WebElement hamburgerPoliciesLink;
     @FindBy(xpath="//form[@id='searchForm']//h2[@class='main__title']")
     public WebElement pageHeading;
     @FindBy(xpath="//div[@class='calendar']/h1[@class='calendar__h']")
     public WebElement calendarHeading;
     @FindBy(xpath="//form[@id='searchForm']//h2[@class='format-summary__desc-header']")
     public WebElement newPageHeading;
+
+
+
     public TheHub(WebDriver driver) {
         TheHub.driver = driver;
         PageFactory.initElements(TheHub.driver, this);
     }
     public void setLoginEmail(String userName){
-        txtBoxLoginEmail.sendKeys(userName);
+        this.txtBoxLoginEmail.sendKeys(userName);
     }
     public void setLoginPassword(String password){
-        txtBoxPassword.sendKeys(password);
+        this.txtBoxPassword.sendKeys(password);
     }
     public void clickOnLoginButton(){
-        btnLogin.click();
-      Assert.assertTrue(this.loginUserImage.isDisplayed());
+        this.btnLogin.click();
+      Assert.assertTrue(loginUserImage.isDisplayed());
     }
     public void enterSearchText(String searchText){
-        txtBoxSearch.sendKeys(searchText);
+        this.txtBoxSearch.sendKeys(searchText);
     }
     public void clickSeachIcon(){
-        btnSearchIcon.click();
-        for (WebElement tab : allResultTabs){
+        this.btnSearchIcon.click();
+        for (WebElement tab : this.allResultTabs){
           //  System.out.println(tab.getText());
 
         }
@@ -94,18 +101,18 @@ public class TheHub {
         }*/
     public void clickOnFilter(String sFacet ) throws InterruptedException {
        // System.out.println(facet.size());
-        for (int i = 0; i< facet.size(); i++){
+        for (int i = 0; i< this.facet.size(); i++){
          //   System.out.println(i);
          //   System.out.println(facet.get(i).getText());
-            if(facet.get(i).getText().contains(sFacet)){
+            if(this.facet.get(i).getText().contains(sFacet)){
          //       System.out.println(facet.get(i).getText());
-                facet.get(i).click();
+                this.facet.get(i).click();
 
                 Thread.sleep(5000);
-               String chosenFacet= selectedFacet();
+               String chosenFacet= this.selectedFacet();
                 System.out.println("chosen : " +chosenFacet.trim());
                 System.out.println("chosen : " +chosenFacet.trim().length());
-                String aFacet[] = facet.get(i).getText().split("\\(");
+                String aFacet[] = this.facet.get(i).getText().split("\\(");
                 if (chosenFacet.trim().equals(aFacet[0].trim())){
                     System.out.println("Awesome");
 
@@ -116,100 +123,124 @@ public class TheHub {
 
 
                 }
-                i = facet.size();
+                i = this.facet.size();
             }
         }
 
 
     }
     public String selectedFacet() {
-        System.out.println(selectedFacet.size());
+        System.out.println(this.selectedFacet.size());
         String chosen = null;
-        for (int j = 0; j < selectedFacet.size(); j++) {
+        for (int j = 0; j < this.selectedFacet.size(); j++) {
             //  System.out.println(this.selectedFacet.get(j).getText());
-           return chosen = selectedFacet.get(j).getText();
+           return chosen = this.selectedFacet.get(j).getText();
         }
 
         return chosen;
     }
 
     public void clickOnLogout(){
-        loginUserImage.click();
-        btnLogout.click();
-        Assert.assertTrue(txtBoxLoginEmail.isDisplayed());
+        this.loginUserImage.click();
+        this.btnLogout.click();
+        Assert.assertTrue(this.txtBoxLoginEmail.isDisplayed());
 
     }
     public void clickOnHamburgerIcon(){
-        this.hamburgerIcon.click();
+        hamburgerIcon.click();
     }
     public void clickonFormatFromHamburgerMenu(){
-        if (hamburgerFormatLink.isDisplayed()){
-            hamburgerFormatLink.click();
-            String pageTitle = this.pageHeading.getText();
+        if (this.hamburgerFormatLink.isDisplayed()){
+            this.hamburgerFormatLink.click();
+            String pageTitle = pageHeading.getText();
             System.out.println(pageTitle);
            Assert.assertTrue(pageTitle.equalsIgnoreCase("Formats"));
         }
         else{
-            Assert.assertFalse(hamburgerFormatLink.isDisplayed(),"format link is not ready to be clicked");
+            Assert.assertFalse(this.hamburgerFormatLink.isDisplayed(),"format link is not ready to be clicked");
         }
     }
     public void clickonSeriesFromHamburgerMenu(){
-        if (hamburgerSeriesLink.isDisplayed()){
-            hamburgerSeriesLink.click();
-            String pageTitle = this.pageHeading.getText();
+        if (this.hamburgerSeriesLink.isDisplayed()){
+            this.hamburgerSeriesLink.click();
+            String pageTitle = pageHeading.getText();
             System.out.println(pageTitle);
             Assert.assertTrue(pageTitle.equalsIgnoreCase("Series"));
         }
         else{
-            Assert.assertFalse(hamburgerFormatLink.isDisplayed(),"Series link is not ready to be clicked");
+            Assert.assertFalse(this.hamburgerFormatLink.isDisplayed(),"Series link is not ready to be clicked");
         }
     }
     public void clickonMediaFromHamburgerMenu(){
 
-        if (hamburgerMediaLink.isDisplayed()){
-            hamburgerMediaLink.click();
-            String pageTitle = this.pageHeading.getText();
+        if (this.hamburgerMediaLink.isDisplayed()){
+            this.hamburgerMediaLink.click();
+            String pageTitle = pageHeading.getText();
             System.out.println(pageTitle);
             Assert.assertTrue(pageTitle.equalsIgnoreCase("Media"));
         }
         else{
-            Assert.assertFalse(hamburgerFormatLink.isDisplayed(),"Series link is not ready to be clicked");
+            Assert.assertFalse(this.hamburgerFormatLink.isDisplayed(),"Series link is not ready to be clicked");
         }
     }
     public void clickonNewsFromHamburgerMenu(){
 
-        if (hamburgerNewsLink.isDisplayed()){
-            hamburgerNewsLink.click();
-            String pageTitle = this.pageHeading.getText();
+        if (this.hamburgerNewsLink.isDisplayed()){
+            this.hamburgerNewsLink.click();
+            String pageTitle = pageHeading.getText();
             System.out.println(pageTitle);
             Assert.assertTrue(pageTitle.equalsIgnoreCase("News"));
         }
         else{
-            Assert.assertFalse(hamburgerFormatLink.isDisplayed(),"Series link is not ready to be clicked");
+            Assert.assertFalse(this.hamburgerFormatLink.isDisplayed(),"Series link is not ready to be clicked");
         }
     }
     public void clickonCalendarFromHamburgerMenu(){
 
-        if (hamburgerCalendarLink.isDisplayed()){
-            hamburgerCalendarLink.click();
-            String pageTitle = this.calendarHeading.getText();
+        if (this.hamburgerCalendarLink.isDisplayed()){
+            this.hamburgerCalendarLink.click();
+            String pageTitle = calendarHeading.getText();
             System.out.println(pageTitle);
             Assert.assertTrue(pageTitle.equalsIgnoreCase("Calendar"));
         }
         else{
-            Assert.assertFalse(hamburgerFormatLink.isDisplayed(),"Series link is not ready to be clicked");
+            Assert.assertFalse(this.hamburgerFormatLink.isDisplayed(),"Series link is not ready to be clicked");
         }
     }
     public void clickonBrandToolkitFromHamburgerMenu(){
 
-        if (hamburgerBrandTookitLink.isDisplayed()){
-            hamburgerBrandTookitLink.click();
-            String pageTitle = this.newPageHeading.getText();
+        if (this.hamburgerBrandTookitLink.isDisplayed()){
+            this.hamburgerBrandTookitLink.click();
+            String pageTitle = newPageHeading.getText();
             System.out.println(pageTitle);
             Assert.assertTrue(pageTitle.equalsIgnoreCase("Brand toolkits"));
         }
         else{
-            Assert.assertFalse(hamburgerBrandTookitLink.isDisplayed(),"Series link is not ready to be clicked");
+            Assert.assertFalse(this.hamburgerBrandTookitLink.isDisplayed(),"Series link is not ready to be clicked");
+        }
+    }
+    public void clickonCommercialFromHamburgerMenu(){
+
+        if (this.hamburgerCommercialLink.isDisplayed()){
+            this.hamburgerCommercialLink.click();
+            String pageTitle = newPageHeading.getText();
+            System.out.println(pageTitle);
+            Assert.assertTrue(pageTitle.equalsIgnoreCase("Commercial"));
+        }
+        else{
+            Assert.assertFalse(this.hamburgerCommercialLink.isDisplayed(),"Series link is not ready to be clicked");
+        }
+    }
+    public void clickonPoliciesFromHamburgerMenu(){
+
+        if (this.hamburgerPoliciesLink.isDisplayed()){
+            this.hamburgerPoliciesLink.click();
+            String pageTitle = newPageHeading.getText();
+            System.out.println(pageTitle);
+            Assert.assertTrue(pageTitle.equalsIgnoreCase("Policies"));
+        }
+        else{
+            Assert.assertFalse(this.hamburgerPoliciesLink.isDisplayed(),"Series link is not ready to be clicked");
         }
     }
 }
---
+++ src/test/java/testcases/clickOnCommercialFromHamburgerMenu.java
@@ -0,0 +1,28 @@
+package testcases;
+
+import PageObjects.TheHub;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by syam.suryanarayanan on 4/12/2017.
+ */
+public class clickOnCommercialFromHamburgerMenu {
+    @Test
+    public void testClickCommercialFromHamburgerMenu(){
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        TheHub theHub = new TheHub(driver);
+        driver.get(TheHub.theHubUrl);
+        driver.manage().window().maximize();
+        theHub.setLoginEmail(TheHub.userName);
+        theHub.setLoginPassword(TheHub.password);
+        theHub.clickOnLoginButton();
+        theHub.clickOnHamburgerIcon();
+        theHub.clickonCommercialFromHamburgerMenu();
+        driver.quit();
+
+    }
+
+}
