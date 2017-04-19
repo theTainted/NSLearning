d3789cb46c8c6b1d45bb76e3307145af156e64da
syam.s@outlook.com <Liverp00l>
2017-04-19 10:28:41 +0200

    Added method FilterSearchResultsSingleSelectJava

--- src/test/java/testcases/FilterSearchResultsSingleSelect.java
+++ src/test/java/testcases/FilterSearchResultsSingleSelect.java
@@ -21,22 +21,7 @@ public class FilterSearchResultsSingleSelect {
       driver.manage().window().maximize();
       theHub.setLoginEmail(TheHub.userName);
       theHub.setLoginPassword(TheHub.password);
-      theHub.clickOnLoginButton();
-   /*  driver.findElement(By.id("q")).sendKeys("alma santo");
-      driver.findElement(By.xpath("//div[@id='search']//button[@class='form-search__btn']")).click();*/
-   /*    List <WebElement> hamburgerMenu = new ArrayList<>();
-       if( driver.findElement(By.xpath("//i[@class='burger-icon']")).isEnabled()){
-            driver.findElement(By.xpath("//i[@class='burger-icon']")).click();
-           List<WebElement> links = driver.findElements(By.xpath("//ul[@class='nav-2016__list']//a"));
-             for (WebElement link : links){
 
-        hamburgerMenu.add(link);
-          }
-           System.out.println(hamburgerMenu.size());
-           for (int j=0; j<hamburgerMenu.size();j++){
-               System.out.println(hamburgerMenu.get(j).getText());
-           }
-       }*/
        theHub.enterSearchText("Hunted");
 
      //  theHub.clickOnAFacet();
