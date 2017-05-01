3f2b1f46e25e441418735d2aa9d2faef27e19da0
syam.s@outlook.com <Liverp00l>
2017-05-01 08:53:06 +0200

    commented the marionette driver import

--- src/test/java/testcases/clickOnBrandToolkitFromHamburgerMenu.java
+++ src/test/java/testcases/clickOnBrandToolkitFromHamburgerMenu.java
@@ -1,5 +1,6 @@
 package testcases;
 
+import PageObjects.General;
 import PageObjects.TheHub;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,17 +12,18 @@ import org.testng.annotations.Test;
 public class clickOnBrandToolkitFromHamburgerMenu {
     @Test
     public void testClickBrandToolkitFromHamburgerMenu(){
-        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        TheHub theHub = new TheHub(driver);
-        driver.get(TheHub.theHubUrl);
-        driver.manage().window().maximize();
+        General.getBrowser("Chrome");
+        /*System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();*/
+        TheHub theHub = new TheHub(General.driver);
+        General.driver.get(TheHub.theHubUrl);
+        General.driver.manage().window().maximize();
         theHub.setLoginEmail(TheHub.userName);
         theHub.setLoginPassword(TheHub.password);
         theHub.clickOnLoginButton();
         theHub.clickOnHamburgerIcon();
         theHub.clickonBrandToolkitFromHamburgerMenu();
-        driver.quit();
+        General.driver.quit();
 
     }
 
