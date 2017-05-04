944538fdebb0347fe60cbfa340cfdfee96b88750
syam.s@outlook.com <Liverp00l>
2017-05-04 15:20:36 +0200

    Now when i run in parallel i see the error error message org.openqa.selenium.NoSuchSessionException: Session ID is null. Using WebDriver after calling quit()? Fixed by changing the annotation BeforeTest to BeforeClass

--- src/test/java/testcases/clickOnCalendarFromHamburgerMenu.java
+++ src/test/java/testcases/clickOnCalendarFromHamburgerMenu.java
@@ -1,5 +1,6 @@
 package testcases;
 
+import PageObjects.General;
 import PageObjects.TheHub;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,20 +9,19 @@ import org.testng.annotations.Test;
 /**
  * Created by syam.suryanarayanan on 4/12/2017.
  */
-public class clickOnCalendarFromHamburgerMenu {
+public class clickOnCalendarFromHamburgerMenu extends General {
     @Test
     public void testClickCalendarFromHamburgerMenu(){
-        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        TheHub theHub = new TheHub(driver);
-        driver.get(TheHub.theHubUrl);
-        driver.manage().window().maximize();
+
+        TheHub theHub = new TheHub(General.driver);
+        General.driver.get(TheHub.theHubUrl);
+        General.driver.manage().window().maximize();
         theHub.setLoginEmail(TheHub.userName);
         theHub.setLoginPassword(TheHub.password);
         theHub.clickOnLoginButton();
         theHub.clickOnHamburgerIcon();
         theHub.clickonCalendarFromHamburgerMenu();
-        driver.quit();
+        General.driver.quit();
 
     }
 
