f11dfa7020aafae82799177e738a16b9930ea6a8
syam.s@outlook.com <Liverp00l>
2017-05-04 08:12:16 +0200

    Trying to setup a multipbrowser running

--- src/test/java/testcases/theHubLogIn.java
+++ src/test/java/testcases/theHubLogIn.java
@@ -4,6 +4,8 @@ import PageObjects.General;
 import PageObjects.TheHub;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 /**
@@ -12,7 +14,7 @@ import org.testng.annotations.Test;
 public class theHubLogIn {
   @BeforeTest
   @Parameters("browser")
-
+  General.getBrowser()
 
   @Test
     public void testTheHubLogin(){
