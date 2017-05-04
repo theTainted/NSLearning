c3671e273ef46cfcdb57859a416c3ae92c336975
syam.s@outlook.com <Liverp00l>
2017-05-04 10:53:16 +0200

    For some obscene reason in IE the password is typed in twice. - fixing by adding clear as it was prefilling the password. In FF for some reason it doesnot enter the username and password - worked with an 0.16 version of gecko driver

--- src/test/java/testcases/theHubLogIn.java
+++ src/test/java/testcases/theHubLogIn.java
@@ -1,15 +1,15 @@
 package testcases;
 
-import PageObjects.General;
-import PageObjects.TheHub;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+        import PageObjects.General;
+        import PageObjects.TheHub;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.ie.InternetExplorerDriver;
+        import org.testng.annotations.BeforeTest;
+        import org.testng.annotations.Parameters;
+        import org.testng.annotations.Test;
 
-import static PageObjects.General.driver;
+        import static PageObjects.General.driver;
 
 /**
  * Created by syam.suryanarayanan on 4/6/2017.
@@ -17,18 +17,18 @@ import static PageObjects.General.driver;
 public class theHubLogIn  extends General {
 
 
-  @Test
+    @Test
     public void testTheHubLogin(){
       /*System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-      WebDriver driver = new ChromeDriver();*/
-   System.setProperty("webdriver.ie.driver","C:/IEDriverServer.exe");
-      General.driver = new InternetExplorerDriver();
-      TheHub theHub = new TheHub(General.driver);
-      General.driver.get(TheHub.theHubUrl);
-      General.driver.manage().window().maximize();
-      theHub.setLoginEmail(TheHub.userName);
-      theHub.setLoginPassword(TheHub.password);
-    //  theHub.clickOnLoginButton();
-      //General.driver.quit();
-  }
+      WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.ie.driver","C:/IEDriverServer.exe");
+        General.driver = new InternetExplorerDriver();*/
+        TheHub theHub = new TheHub(General.driver);
+        General.driver.get(TheHub.theHubUrl);
+        General.driver.manage().window().maximize();
+        theHub.setLoginEmail(TheHub.userName);
+        theHub.setLoginPassword(TheHub.password);
+        theHub.clickOnLoginButton();
+        General.driver.quit();
+    }
 }
