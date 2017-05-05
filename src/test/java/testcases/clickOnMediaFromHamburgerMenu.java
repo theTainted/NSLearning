3ce08b1045521900628c30a166dae7e06411a269
syam.s@outlook.com <Liverp00l>
2017-05-05 10:04:16 +0200

    <class name="testcases.clickOnMediaFromHamburgerMenu"/>

--- src/test/java/testcases/clickOnMediaFromHamburgerMenu.java
+++ src/test/java/testcases/clickOnMediaFromHamburgerMenu.java
@@ -1,18 +1,19 @@
 package testcases;
 
+import PageObjects.General;
 import PageObjects.TheHub;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 /**
  * Created by syam.suryanarayanan on 4/12/2017.
  */
-public class clickOnMediaFromHamburgerMenu {
+public class clickOnMediaFromHamburgerMenu extends General{
     @Test
     public void testClickMediaFromHamburgerMenu(){
-        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+
         TheHub theHub = new TheHub(driver);
         driver.get(TheHub.theHubUrl);
         driver.manage().window().maximize();
@@ -21,8 +22,11 @@ public class clickOnMediaFromHamburgerMenu {
         theHub.clickOnLoginButton();
         theHub.clickOnHamburgerIcon();
         theHub.clickonMediaFromHamburgerMenu();
-        driver.quit();
 
+        }
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
     }
 
 }
