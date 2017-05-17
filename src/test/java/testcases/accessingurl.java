3635272a3199ce58c89329c45caa8327fe5873c2
syam.s@outlook.com <Liverp00l>
2017-05-17 14:37:32 +0200

    tried creating  a script which reads a file and passes the url and then accesses it and then writes the currentUrl to a file

---
+++ src/test/java/testcases/accessingurl.java
@@ -0,0 +1,54 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.*;
+
+/**
+ * Created by syam.suryanarayanan on 5/17/2017.
+ */
+public class accessingurl {
+
+    public static void main(String [] args) throws IOException {
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+      //  WebDriver driver = new ChromeDriver();
+         String fileName = "C:\\filename22.txt";
+
+
+
+        BufferedReader br = null;
+
+
+
+            String sCurrentLine;
+
+            br = new BufferedReader(new FileReader("C:\\url1.txt"));
+        FileWriter fw = new FileWriter(fileName,true);
+            while ((sCurrentLine = br.readLine()) != null) {
+                //System.out.println(sCurrentLine);
+                WebDriver driver = new ChromeDriver();
+                driver.get(sCurrentLine);
+               // System.out.println(driver.getCurrentUrl());
+                //if(driver.getCurrentUrl().contains("https")){
+                    //System.out.println("redirects to https");
+
+
+
+
+                    String sCurrentUrl=driver.getCurrentUrl();
+                System.out.println(sCurrentLine + ":"+sCurrentUrl);
+                    fw.write(sCurrentLine + ":"+sCurrentUrl);
+                 driver.quit();
+
+
+                }
+
+        fw.flush();
+        fw.close();
+
+        }
+
+    }
+
+
