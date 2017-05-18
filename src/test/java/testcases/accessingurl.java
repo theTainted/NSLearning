8f8ae58fef9499e9be12375ce2cdac31e69517af
syam.s@outlook.com <Liverp00l>
2017-05-18 11:46:58 +0200

    this only writes the last line to the text file

--- src/test/java/testcases/accessingurl.java
+++ src/test/java/testcases/accessingurl.java
@@ -13,43 +13,33 @@ public class accessingurl {
     public static void main(String [] args) throws IOException {
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
       //  WebDriver driver = new ChromeDriver();
-         String fileName = "C:\\filename222.txt";
+         String fileName = "C:\\asdf.txt";
         File f = new File(fileName);
         if(!f.exists()) {
             f.createNewFile();
         }
 
         BufferedReader br = null;
-        BufferedWriter bw = null;
-
-
-            String sCurrentLine;
-
-            br = new BufferedReader(new FileReader("\\..\\url1.txt"));
-        FileWriter fw = new FileWriter(fileName);
-
-
+      //  BufferedWriter bw = null;
+         String sCurrentLine;
+         br = new BufferedReader(new FileReader("\\..\\url1.txt"));
+         FileWriter fw = new FileWriter(fileName);
             while ((sCurrentLine = br.readLine()) != null) {
-                //System.out.println(sCurrentLine);
                 WebDriver driver = new ChromeDriver();
                 driver.get(sCurrentLine);
-
-                    String sCurrentUrl=driver.getCurrentUrl();
-                    fw.write(sCurrentLine + " : "+sCurrentUrl);
-
-               /* fw = new FileWriter(fileName);
-                bw = new BufferedWriter(fw);
+                String sCurrentUrl=driver.getCurrentUrl();
+                // fw.write(sCurrentLine + " : "+sCurrentUrl);
+             fw = new FileWriter(fileName);
+                BufferedWriter  bw = new BufferedWriter(fw);
                 bw.write(sCurrentLine + " : "+sCurrentUrl);
-                //bw.newLine();*/
+                bw.flush();
+                driver.quit();
 
-
-                 driver.quit();
 
 
                 }
 
-        fw.flush();
-        fw.close();
+
 
         }
 
