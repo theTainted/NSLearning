96043471f2a85bce1d0ad111fdab352c6225a284
syam.s@outlook.com <Liverp00l>
2017-05-31 07:42:36 +0200

    Added MockTest for the balanced array problem

---
+++ src/test/java/testcases/MockTest.java
@@ -0,0 +1,22 @@
+package testcases;
+
+/**
+ * Created by syam.suryanarayanan on 5/31/2017.
+ */
+public class MockTest {
+    public static void main(String [] args){
+        int testArray[] ={1,2,3};
+        int sum = 0;
+        for (int i = 0; i < testArray.length; i++) {
+            sum = sum + testArray[i];
+        }
+        // If sum is odd, there cannot be two subsets
+        // with equal sum
+        if (sum%2 == 0){
+            System.out.println("the array can be partitioned to two");
+        }
+        else{
+            System.out.println("Cannot be partitioned");
+        }
+    }
+}
