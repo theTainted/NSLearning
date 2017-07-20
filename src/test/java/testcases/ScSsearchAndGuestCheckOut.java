dee24b576d80a8f3bfdedb4c3a66e7a8d8438d9d
syam.s@outlook.com <Liverp00l>
2017-07-20 08:22:24 +0200

    updated the guestCheckoutFlow to handle the popup to (click and use the unregistered workflow). Have not tested.

--- src/test/java/testcases/ScSsearchAndGuestCheckOut.java
+++ src/test/java/testcases/ScSsearchAndGuestCheckOut.java
@@ -1,7 +1,24 @@
 package testcases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by syam.suryanarayanan on 7/20/2017.
  */
 public class ScSsearchAndGuestCheckOut {
+    public static void main (String[] args){
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String sURL = "https://staging.scs.co.uk";
+        String sProductName ="L036211";
+        driver.get(sURL);
+        driver.manage().window().maximize();
+        driver.findElement(By.id("q")).sendKeys(sProductName);
+        driver.findElement(By.id("q")).sendKeys(Keys.RETURN);
+
+
+    }
 }
