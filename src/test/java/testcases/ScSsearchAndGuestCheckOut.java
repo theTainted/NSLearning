32b1cb590d3af2b1a6121623b351dd83e1c20cf7
syam.s@outlook.com <Liverp00l>
2017-07-20 08:36:58 +0200

    updated the guestCheckoutFlow to handle the popup to (click and use the unregistered workflow). Have not tested.

--- src/test/java/testcases/ScSsearchAndGuestCheckOut.java
+++ src/test/java/testcases/ScSsearchAndGuestCheckOut.java
@@ -2,6 +2,7 @@ package testcases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -18,6 +19,11 @@ public class ScSsearchAndGuestCheckOut {
         driver.manage().window().maximize();
         driver.findElement(By.id("q")).sendKeys(sProductName);
         driver.findElement(By.id("q")).sendKeys(Keys.RETURN);
+        System.out.println(driver.findElements(By.xpath("//input[@id='add-to-cart']")).size());
+        boolean elementExists = driver.findElements(By.id("add-to-cart")).size()!=0;
+        if (elementExists==true){
+            driver.findElement(By.id("add-to-cart")).click();
+        }
 
 
     }
