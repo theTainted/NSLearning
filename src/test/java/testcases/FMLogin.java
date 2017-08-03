22b2af2316a083bbfff1b3c891c824c42841b9f8
syam.s@outlook.com <Liverp00l>
2017-08-03 07:12:55 +0200

    PoC for FM

--- src/test/java/testcases/FMLogin.java
+++ src/test/java/testcases/FMLogin.java
@@ -1,4 +1,30 @@
 package testcases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class FMLogin {
+    public static void main (String [] args){
+        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String sURL = "http://t2.fleurametz.com";
+        String sEmail ="mobilexl@fleurametz.com";
+        String sPassword= "lelie";
+        driver.get(sURL);
+        driver.manage().window().maximize();
+        driver.findElement(By.id("email")).sendKeys(sEmail);
+        driver.findElement(By.id("password")).sendKeys(sPassword);
+        driver.findElement(By.xpath("//input[@class='btn btn--login btn-login']")).click();
+        if((driver.findElement(By.xpath("//li[3]//a[@class='dropdown-text']")).getText()).equals(sEmail)) {
+
+            System.out.println("true");
+        }
+
+        List<WebElement> deliveryDateHasOrder = driver.findElements(By.xpath("//div[@id='start-new-order-calendar']/div/form//td[@class='delivery-order has-order']/a"));
+        System.out.println(deliveryDateHasOrder.size());
+    }
 }
