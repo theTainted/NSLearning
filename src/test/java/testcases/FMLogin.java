d5f24ec945c8839977b4a5cb62dc4eba0a75b843
syam.s@outlook.com <Liverp00l>
2017-08-08 08:44:29 +0200

    List<WebElement> possibleDeliveryDatesWithNoOrders = driver.findElements(By.xpath("//div[@id='start-new-order-calendar']//form//table//td[@class='delivery']"));         System.out.println(possibleDeliveryDatesWithNoOrders.size());

--- src/test/java/testcases/FMLogin.java
+++ src/test/java/testcases/FMLogin.java
@@ -8,12 +8,12 @@ package testcases;
         import java.util.List;
 
 public class FMLogin {
-    public static void main (String [] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         String sURL = "http://t2.fleurametz.com";
-        String sEmail ="mobilexl@fleurametz.com";
-        String sPassword= "lelie";
+        String sEmail = "mobilexl@fleurametz.com";
+        String sPassword = "lelie";
         driver.get(sURL);
         driver.manage().window().maximize();
         driver.findElement(By.id("email")).sendKeys(sEmail);
@@ -27,7 +27,11 @@ public class FMLogin {
         List<WebElement> deliveryDateHasOrder = driver.findElements(By.xpath("//div[@id='start-new-order-calendar']/div/form//td[@class='delivery-order has-order']/a"));
         System.out.println(deliveryDateHasOrder.size());
         Thread.sleep(2000);
-        List<WebElement> deliveryDateTodayHasOrder =driver.findElements(By.xpath("//div[@id='start-new-order-calendar']//form//table//td[@class='delivery-order active has-order today']"));
+        List<WebElement> deliveryDateTodayHasOrder = driver.findElements(By.xpath("//div[@id='start-new-order-calendar']//form//table//td[@class='delivery-order active has-order today']"));
         System.out.println(deliveryDateTodayHasOrder.size());
+        List<WebElement> possibleDeliveryDatesWithNoOrders = driver.findElements(By.xpath("//div[@id='start-new-order-calendar']//form//table//td[@class='delivery']"));
+        System.out.println(possibleDeliveryDatesWithNoOrders.size());
     }
 }
+
+
