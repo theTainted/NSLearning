be5dcd6a3d5c9002137f6b8941706646d6026fcc
syam.s@outlook.com <Liverp00l>
2017-08-24 16:58:44 +0200

    added clickOnChkBoxes()

--- src/test/java/PageObjects/scsOrderPreview.java
+++ src/test/java/PageObjects/scsOrderPreview.java
@@ -1,10 +1,14 @@
 package PageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.asserts.SoftAssert;
 
 public class scsOrderPreview extends General{
+    @FindBy(xpath="//label[@for='dwfrm_summary_summaryCheckboxes_readTermsConditions']")
+    public WebElement chkBoxTerms;
 
         //  WebDriver driver;
         SoftAssert softAssert = new SoftAssert();
@@ -12,5 +16,9 @@ public class scsOrderPreview extends General{
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
     }
+     public void clickOnChkBoxTerm(){
+        System.out.println(chkBoxTerms.getAttribute("value"));
+        chkBoxTerms.click();
 
+     }
     }
