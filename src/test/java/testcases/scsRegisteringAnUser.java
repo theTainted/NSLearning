0091362a4ef2d04ca94efc2143d5f0b555684ab1
syam.s@outlook.com <Liverp00l>
2017-08-25 14:40:36 +0200

    created scsRegisteringAnUser

--- src/test/java/testcases/scsRegisteringAnUser.java
+++ src/test/java/testcases/scsRegisteringAnUser.java
@@ -1,4 +1,17 @@
 package testcases;
 
-public class scsRegisteringAnUser {
+import PageObjects.General;
+import org.testng.annotations.Test;
+
+public class scsRegisteringAnUser extends General {
+
+    @Test
+
+    public void testScSRegisteringAnUser(){
+
+        driver.get(General.URL);
+        driver.manage().window().maximize();
+
+
+    }
 }
