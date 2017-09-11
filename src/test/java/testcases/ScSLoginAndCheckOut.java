41cf2c896485b34e7f493562461ba358212ffbfa
syam.s@outlook.com <Liverp00l>
2017-09-11 16:41:57 +0200

    Convert the below into functions
    boolean elementExists = driver.findElements(By.xpath("//input[@class='add-to-cart btn btn--buy btn--large btn--full']")).size() != 0;
            if (elementExists == true) {
                driver.findElement(By.xpath("//input[@class='add-to-cart btn btn--buy btn--large btn--full']")).click();
            }
            Thread.sleep(3000);
            // driver.switchTo().frame(1);
            boolean windowContinueToCheckOutButtonExists = driver.findElements(By.xpath("//div[@id='dialog--extra-products']//div[@class='dialog--button--bar']/a[1]")).size() != 0;
            if (windowContinueToCheckOutButtonExists == true) {
                driver.findElement(By.xpath("//div[@id='dialog--extra-products']//div[@class='dialog--button--bar']/a[1]")).click();
            }
            boolean cookieButtonExists = driver.findElements(By.xpath("//div[@class='cookie-policy__container shown']/span[@class='icon-close']")).size() != 0;
            if (cookieButtonExists) {
                driver.findElement(By.xpath("//div[@class='cookie-policy__container shown']/span[@class='icon-close']")).click();
            }

--- src/test/java/testcases/ScSLoginAndCheckOut.java
+++ src/test/java/testcases/ScSLoginAndCheckOut.java
@@ -1,6 +1,7 @@
 package testcases;
 
         import PageObjects.*;
+        import org.openqa.selenium.By;
         import org.testng.annotations.Test;
 
         import static PageObjects.General.driver;
@@ -20,6 +21,21 @@ public class ScSLoginAndCheckOut {
         login.clickOnRegisteredLogin();
 
         ProductListPage plp = homePage.searchText(sProductName);
+
+        boolean elementExists = driver.findElements(By.xpath("//input[@class='add-to-cart btn btn--buy btn--large btn--full']")).size() != 0;
+        if (elementExists == true) {
+            driver.findElement(By.xpath("//input[@class='add-to-cart btn btn--buy btn--large btn--full']")).click();
+        }
+        Thread.sleep(3000);
+        // driver.switchTo().frame(1);
+        boolean windowContinueToCheckOutButtonExists = driver.findElements(By.xpath("//div[@id='dialog--extra-products']//div[@class='dialog--button--bar']/a[1]")).size() != 0;
+        if (windowContinueToCheckOutButtonExists == true) {
+            driver.findElement(By.xpath("//div[@id='dialog--extra-products']//div[@class='dialog--button--bar']/a[1]")).click();
+        }
+        boolean cookieButtonExists = driver.findElements(By.xpath("//div[@class='cookie-policy__container shown']/span[@class='icon-close']")).size() != 0;
+        if (cookieButtonExists) {
+            driver.findElement(By.xpath("//div[@class='cookie-policy__container shown']/span[@class='icon-close']")).click();
+        }
 
     }
 }
