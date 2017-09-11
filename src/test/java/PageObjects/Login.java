0834f5329b8eafa3195d7fd970b4d4f425f88c3e
syam.s@outlook.com <Liverp00l>
2017-09-11 16:06:59 +0200

    added the login part

--- src/test/java/PageObjects/Login.java
+++ src/test/java/PageObjects/Login.java
@@ -10,52 +10,56 @@ import org.testng.asserts.SoftAssert;
  * Created by syam.suryanarayanan on 11/17/2016.
  */
 public class Login extends General {
-  //  WebDriver driver;
+    //  WebDriver driver;
     SoftAssert softAssert = new SoftAssert();
 
-    @FindBy(xpath="//div[@id='checkoutloginpage']//button[@name='dwfrm_login_unregistered']")
+    @FindBy(xpath = "//div[@id='checkoutloginpage']//button[@name='dwfrm_login_unregistered']")
     public WebElement btnCheckoutAsGuest;
-    @FindBy(xpath="//input[contains(@id,'dwfrm_login_username')]")
+    @FindBy(xpath = "//input[contains(@id,'dwfrm_login_username')]")
     public WebElement username;
-    @FindBy(xpath="//input[contains(@id,'dwfrm_login_password')]")
+    @FindBy(xpath = "//input[contains(@id,'dwfrm_login_password')]")
     public WebElement txtPassword;
-    @FindBy(xpath="//form[@id='dwfrm_login']//button[@name='dwfrm_login_login']")
+    @FindBy(xpath = "//form[@id='dwfrm_login']//button[@name='dwfrm_login_login']")
     public WebElement btnLogin;
-  //  @FindBy(xpath="//div[@id='checkoutloginpage']//button[@name='dwfrm_login_register']")
-    @FindBy(xpath="//form[@id='dwfrm_login_register']//button[@value='Create an Account']") //->updated for SCS
+    //  @FindBy(xpath="//div[@id='checkoutloginpage']//button[@name='dwfrm_login_register']")
+    @FindBy(xpath = "//form[@id='dwfrm_login_register']//button[@value='Create an Account']") //->updated for SCS
     public WebElement btnCreateAccount;
 
-    @FindBy(xpath="//div[@id='checkoutloginpage']//button[@name='dwfrm_login_unregistered']")
+    @FindBy(xpath = "//div[@id='checkoutloginpage']//button[@name='dwfrm_login_unregistered']")
     public WebElement popupUnregisteredCheckIn;
 
-    public Login(WebDriver driver){
-       General.driver= driver;
-        PageFactory.initElements(General.driver,this);
+    public Login(WebDriver driver) {
+        General.driver = driver;
+        PageFactory.initElements(General.driver, this);
     }
 
 
-    public ShippingAddress clickOnCheckoutAsGuest(){
+    public ShippingAddress clickOnCheckoutAsGuest() {
         btnCheckoutAsGuest.click();
         return new ShippingAddress(driver);
 
     }
-   public RegistrationPage clickCreateAccountButton(){
-       btnCreateAccount.click();
-       return new RegistrationPage(driver);
-   }
 
-    public void enterUserName(String emailID){
+    public RegistrationPage clickCreateAccountButton() {
+        btnCreateAccount.click();
+        return new RegistrationPage(driver);
+    }
+
+    public void enterUserName(String emailID) {
         username.sendKeys(emailID);
     }
 
-    public void enterPassword(String password){
+    public void enterPassword(String password) {
         txtPassword.sendKeys(password);
     }
-
+/*
     public ShippingAddress clickOnRegisteredLogin(){
         btnLogin.click();
         return new ShippingAddress(driver);
+    }*/
+
+    public void clickOnRegisteredLogin() {
+        btnLogin.click();
     }
+
 }
-
-
