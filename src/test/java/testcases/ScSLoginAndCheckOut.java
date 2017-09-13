5e735a7fdedc781bdffd6d9cf4de68787af73491
syam.s@outlook.com <Liverp00l>
2017-09-13 11:05:13 +0200

    need to re-write the ScSLogingAndCheckOut.Java

--- src/test/java/testcases/ScSLoginAndCheckOut.java
+++ src/test/java/testcases/ScSLoginAndCheckOut.java
@@ -27,54 +27,5 @@ public class ScSLoginAndCheckOut extends General {
 
         ProductListPage plp = homePage.searchText(sProductName);
 
-        boolean elementExists = driver.findElements(By.xpath("//input[@class='add-to-cart btn btn--buy btn--large btn--full']")).size() != 0;
-        if (elementExists == true) {
-            driver.findElement(By.xpath("//input[@class='add-to-cart btn btn--buy btn--large btn--full']")).click();
-        }
-
-        Thread.sleep(3000);
-        ShoppingBasket sb = new ShoppingBasket(driver);
-
-
-        // driver.switchTo().frame(1);
-        boolean windowContinueToCheckOutButtonExists = driver.findElements(By.xpath("//div[@id='dialog--extra-products']//div[@class='dialog--button--bar']/a[1]")).size() != 0;
-        if (windowContinueToCheckOutButtonExists == true) {
-            driver.findElement(By.xpath("//div[@id='dialog--extra-products']//div[@class='dialog--button--bar']/a[1]")).click();
-        }
-
-        boolean cookieButtonExists = driver.findElements(By.xpath("//div[@class='cookie-policy__container shown']/span[@class='icon-close']")).size() != 0;
-        if (cookieButtonExists) {
-            driver.findElement(By.xpath("//div[@class='cookie-policy__container shown']/span[@class='icon-close']")).click();
-
-        }
-        Thread.sleep(20000);
-        sb.clickOnCloseNewsLetterPopUp();
-            ShippingAddress shippingAddress = new ShippingAddress(driver);
-          driver.findElement(By.xpath("//form[@id='checkout-form']//button[@class='btn btn--pay btn--full shopping-cart-payment__btn dialogify']")).click();
-            shippingAddress.scsSelectTitle("Miss");
-            shippingAddress.enterFirstName("test");
-            shippingAddress.enterLastName("tester");
-            shippingAddress.scsEnterEmail("syam.suryanarayanan@digitaslbi.com");
-            shippingAddress.enterPhone("3333333333");
-            shippingAddress.selectBestTimeToContact("Call me now");
-
-            shippingAddress.scsEnterZip("AB11 5BA");
-            shippingAddress.clickOnLookUp();
-            Thread.sleep(7000);
-            shippingAddress.scsSelectAnAddressFromAddressDropDown(3);
-            Thread.sleep(3000);
-            shippingAddress.clickscsBtnContinueInShippingPage();
-            scsOrderPreview orderPreview = new scsOrderPreview(driver);
-            orderPreview.clickOnChkBoxTerm();
-            orderPreview.clickOnPayByCard();
-        /*
-        BillingPage billingPage = new BillingPage(driver);
-        billingPage.enterNumber("5100290029002909");
-        billingPage.enterCVV("737");
-        billingPage.enterName("test");
-        billingPage.selectExpiryMonth("10");
-        billingPage.selectExpiryYear("2020");
-        billingPage.clickBtnToPay();*/
-        }
-
     }
+}
