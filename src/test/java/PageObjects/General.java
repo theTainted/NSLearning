c49e3b7912b2a31a7c9451ce03d26c22b64b85aa
syam.s@outlook.com <Liverp00l>
2017-10-02 15:57:21 +0200

    in pom.xml updated selenium to version 3.6.0

--- src/test/java/PageObjects/General.java
+++ src/test/java/PageObjects/General.java
@@ -1,6 +1,6 @@
 package PageObjects;
 
-import org.junit.BeforeClass;
+//import org.junit.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
